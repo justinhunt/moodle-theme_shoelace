c3c22e81735b70fb16296e0d4686157f24c63698
Gareth J Barnard <gjbarnard@gmail.com>
2016-03-31 17:24:11 +0100

    Finished?

--- amd/src/scroll.js
+++ amd/src/scroll.js
@@ -13,25 +13,21 @@ define(['jquery', 'core/log'], function($, log) {
         settings.elementHeight = settings.theElement.height() + 1;
         var tally = 0;
         var diff = 0;
-        var currentElementTop = 0;
+        var elementTop = 0;
         var elementFullyShown = true;
         var elementFullyHidden = false;
         var down = false;
         var last = $(this).scrollTop();
         var current = last;
 
-        var inspect = function(object) {
-            for (var key in object) {
-                log.debug('K: ' + key + ' V: ' + object[key]);
-            }
-        };
+        log.debug('shoelaceScroll - Element height + 1: ' + settings.elementHeight);
+        log.debug('shoelaceScroll - Scroll up amount  : ' + settings.scrollupamount);
 
         var setElementTop = function() {
-            var newElementTop = currentElementTop;
             if (down) {
-                newElementTop += diff;
-                if (newElementTop > settings.elementHeight) {
-                    newElementTop = settings.elementHeight;
+                elementTop += diff;
+                if (elementTop > settings.elementHeight) {
+                    elementTop = settings.elementHeight;
                     elementFullyHidden = true;
                     elementFullyShown = false;
                     log.debug('SET: Element fully hidden.');
@@ -41,9 +37,9 @@ define(['jquery', 'core/log'], function($, log) {
                     log.debug('SET: Element partial (down).');
                 }
             } else {
-                newElementTop -= diff;
-                if (newElementTop < 0) {
-                    newElementTop = 0;
+                elementTop -= diff;
+                if (elementTop < 0) {
+                    elementTop = 0;
                     elementFullyShown = true;
                     elementFullyHidden = false;
                     log.debug('SET: Element fully shown.');
@@ -53,25 +49,20 @@ define(['jquery', 'core/log'], function($, log) {
                     log.debug('SET: Element partial (up).');
                 }
             }
-            settings.theElement.css('top', '-' + newElementTop + 'px');
-            currentElementTop = newElementTop;
+            settings.theElement.css('top', '-' + elementTop + 'px');
         }
 
         this.on('mouseup', function (evt) {
             log.debug('MUP - tally reset.');
             tally = 0;
-            //log.debug('MUP ' + evt.pageX);
         });
 
         this.on('touchend', function (evt) {
             log.debug('TEND - tally reset.');
             tally = 0;
-            //log.debug('TENDP ' + evt.pageX);
         });
 
         this.on('keyup', function (evt) {
-            //log.debug('KEYP ' + evt.pageX);
-            //log.debug('KEYC ' + evt.keyCode);
             log.debug('KEYC ' + evt.which);
             // Key 38 is cursor up and ket 40 is cursor down.
             if ((evt.which == 38) || (evt.which == 40)) {
@@ -80,19 +71,14 @@ define(['jquery', 'core/log'], function($, log) {
                     tally = 0;
                 }
             }
-            //inspect(evt);
         });
 
         this.on('resize', function (evt) {
             log.debug('RESZ - tally reset.');
             tally = 0;
-            //inspect(evt);
         });
 
         this.scroll(function(evt) {
-            //log.debug('SC');
-            //log.debug('SC EVT:');
-            //inspect(evt.originalEvent);
             current = $(this).scrollTop();
             log.debug('SCTop: ' + current);
             if (current > last) {
@@ -116,7 +102,7 @@ define(['jquery', 'core/log'], function($, log) {
                 down = false;
                 diff = last - current;
                 tally += diff;
-                if ((tally >= settings.move) || (current <= settings.elementHeight)) {
+                if ((tally >= settings.scrollupamount) || (current <= settings.elementHeight)) {
                     // Start to show the element if we have moved beyond the tally or getting to the element height at the top.
                     if (!elementFullyShown) {
                         setElementTop();
@@ -132,16 +118,18 @@ define(['jquery', 'core/log'], function($, log) {
     };
     
     $.fn.shoelaceScroll.defaults = {
-        move: 250
+        scrollupamount: 250
     };
 }($));
 
     return {
-        init: function() {
+        init: function(data) {
             log.debug('Shoelace Scroll AMD init.');
             $(document).ready(function($) {
-                
-                $(window).shoelaceScroll({theElement: $('.navbar')});
+                $(window).shoelaceScroll({
+                    theElement: $('.navbar'),
+                    scrollupamount: data.navbarscrollupamount
+                });
             });
         }
     }
